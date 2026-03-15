c88844fca939dc94d6423080a0124c219a3796bb
Dishita <DISHITA.26434@stu.upes.ac.in>
2026-03-15 23:34:38 +0530

    Create day42.c

---
+++ day42.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    int queue[1000];
+    int stack[1000];
+
+    for(int i=0;i<n;i++)
+        scanf("%d",&queue[i]);
+
+    int top=-1;
+
+    for(int i=0;i<n;i++)
+        stack[++top]=queue[i];
+
+    for(int i=0;i<n;i++)
+        queue[i]=stack[top--];
+
+    for(int i=0;i<n;i++)
+        printf("%d ",queue[i]);
+
+    return 0;
+}
