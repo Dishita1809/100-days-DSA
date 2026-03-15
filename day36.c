6c509d6d2c471d0937b41684a008f566d345534b
Dishita <DISHITA.26434@stu.upes.ac.in>
2026-03-15 23:04:06 +0530

    Create day36.c

---
+++ day36.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,m;
+    scanf("%d",&n);
+
+    int q[1000];
+
+    for(int i=0;i<n;i++)
+        scanf("%d",&q[i]);
+
+    scanf("%d",&m);
+
+    int front = 0;
+
+    front = (front + m) % n;
+
+    for(int i=0;i<n;i++)
+        printf("%d ", q[(front+i)%n]);
+
+    return 0;
+}
