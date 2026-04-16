d028861244a84927cb71f21fad5c604d89cb4144
Dishita <DISHITA.26434@stu.upes.ac.in>
2026-04-16 08:58:01 +0530

    Create day74.c

---
+++ day74.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    char names[n][100];
+    char unique[n][100];
+    int count[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%s", names[i]);
+    }
+
+    int size = 0;
+
+    for (int i = 0; i < n; i++) {
+        int found = -1;
+
+        for (int j = 0; j < size; j++) {
+            if (strcmp(names[i], unique[j]) == 0) {
+                found = j;
+                break;
+            }
+        }
+
+        if (found != -1) {
+            count[found]++;
+        } else {
+            strcpy(unique[size], names[i]);
+            count[size] = 1;
+            size++;
+        }
+    }
+
+    int maxVotes = 0;
+    char winner[100];
+
+    for (int i = 0; i < size; i++) {
+        if (count[i] > maxVotes) {
+            maxVotes = count[i];
+            strcpy(winner, unique[i]);
+        } else if (count[i] == maxVotes) {
+            if (strcmp(unique[i], winner) < 0) {
+                strcpy(winner, unique[i]);
+            }
+        }
+    }
+
+    printf("%s %d", winner, maxVotes);
+
+    return 0;
+}
